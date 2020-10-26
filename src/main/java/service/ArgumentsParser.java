333d08c54a5b29159123da74b7f158704bd11f2f
Patricio Tula <tula.patricio@gmail.com>
2020-10-26 12:35:37 +0100

    Error message and clean up ArgumentsParser (#29)

--- src/main/java/service/ArgumentsParser.java
+++ src/main/java/service/ArgumentsParser.java
@@ -2,12 +2,15 @@ package service;
 
 public abstract class ArgumentsParser {
 
+    private static final int APP_ARGUMENTS_NUM = 1;
+
     private ArgumentsParser() {
     }
 
     public static AppArguments parseArguments(String[] args) {
-        if (args.length != 1) {
-            throw new IllegalArgumentException();
+        if ( args.length != APP_ARGUMENTS_NUM) {
+            throw new IllegalArgumentException(String.format(
+                    "Invalid number of arguments, maximum is %d and minimum is %d", APP_ARGUMENTS_NUM));
         }
         return new AppArguments(args[0]);
     }
