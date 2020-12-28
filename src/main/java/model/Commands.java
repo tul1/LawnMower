594ec3a02e79554071201e5eaf9693982bb7a8e3
Patricio Tula <tula.patricio@gmail.com>
2020-12-28 13:46:20 +0100

    fixed typo and added an alternative way to run the LawnMower (#40) (#41)

--- src/main/java/model/Commands.java
+++ src/main/java/model/Commands.java
@@ -1,6 +1,11 @@
 package model;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 
 public class Commands {
 
